fab5918a937aaaa86747eb55ca9521e6106b611a
Nirator78 <51956876+Nirator78@users.noreply.github.com>
2022-09-21 13:42:22 +0200

    refacto mineur generateur de csv

--- generate_csv_hour_total_per_game.py
+++ generate_csv_hour_total_per_game.py
@@ -4,21 +4,16 @@ from os.path import isfile, join
 
 df = pd.DataFrame([], columns=['appid', 'time'])
 
-print(df)
-
 listeFichiers = ["data/PlayerCountHistory/" + f for f in listdir("data/PlayerCountHistory") if isfile(join("data/PlayerCountHistory", f))]
-# print(listeFichiers)
+
 for file in listeFichiers:
     print(file)
-    print(listeFichiers[0])
     fileDataframe = pd.read_csv(file, sep=",", header="infer", encoding="Latin-1")
-    Total = fileDataframe['Playercount'].sum()
-    print(file.split("/")[2].split(".")[0])
+    total = fileDataframe['Playercount'].sum()
+
     appid = file.split("/")[2].split(".")[0]
-    print(Total)
-    df2 = pd.DataFrame([[appid, Total]], columns=['appid', 'time'])
-    print(df2)
+
+    df2 = pd.DataFrame([[appid, total]], columns=['appid', 'time'])
     df = pd.concat([df, df2], ignore_index=True)
 
-print(df)
-df.to_csv('out.csv', index=False)
+df.to_csv('data/hours_total_per_game.csv', index=False)
