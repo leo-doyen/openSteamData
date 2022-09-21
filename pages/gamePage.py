ba2edbe566f59e88e41810072b6ca02a33f0a031
leo-doyen <leodoyen78@gmail.com>
2022-09-21 11:47:24 +0200

    feat : first filter + and price display for the game

---
+++ pages/gamePage.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+
+st.markdown("# Evolution des heures de jeux au cours du temps")
+
+# Chargement des données
+df = pd.read_csv("data/applicationInformation.csv", sep=",", header="infer", encoding="Latin-1")
+# Conversion de l"id en string
+df["appid"] = df["appid"].astype(str)
+# Ajout d"une colonne avec le nom du jeu et son id
+df["title"] = df["name"] + "|" + df["appid"]
+# On affiche la liste des jeux pour filtrer
+option = st.selectbox("Quelle jeu voulez vous voir ?", df["title"])
+# Résultat du filtre
+st.write("Jeux sélectionné : ", option.split("|")[1] , option.split("|")[0])
+
+appName = option.split("|")[0]
+appId = option.split("|")[1]
+
+if appId != "" and appName != "":
+    if st.button("Afficher les graphiques"):
+        st.markdown("## Graphique des heures de jeux : " + appName)
+        # On récupère le fichier des heures de jeux du jeu sélectionné
+        fichierLocation = "data/PriceHistory/" + appId + ".csv"
+        df = pd.read_csv(fichierLocation, sep=",", header="infer", encoding="Latin-1")
+        st.dataframe(df)
+        st.line_chart(df, x='Date', y=['Finalprice','Initialprice'])
