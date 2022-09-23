857088aca2089380f92cf9173b40f3f73a55137a
leo-doyen <leodoyen78@gmail.com>
2022-09-23 15:15:16 +0200

    chore : grammatical failure

--- 🏠_Accueil.py
+++ 🏠_Accueil.py
@@ -7,7 +7,7 @@ st.set_page_config(
 
 st.markdown("![](https://indigobuzz.fr/wp-content/uploads/2018/11/steam-logo.jpg)")
 st.markdown("# Accueil")
-st.success("Cette application est en efin en production proche de chez vous.")
+st.success("Cette application est en enfin en production proche de chez vous.")
 st.warning('Certaines pages peuvent ne pas fonctionner correctement si vous rencontrer un problème contacter Guillaume au 07 61 77 77 84.')
 
 st.write("## Open Data Steam")
